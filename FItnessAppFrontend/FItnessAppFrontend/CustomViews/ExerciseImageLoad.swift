90131d7d44567a7cb294753e95748beb70fb4fb8
SudeshikaNG <sng2.sudeshikang@gmail.com>
2023-05-19 13:10:13 +0530

    imae loading done, exercise details loading done

---
+++ FItnessAppFrontend/FItnessAppFrontend/CustomViews/ExerciseImageLoad.swift
@@ -0,0 +1,20 @@
+//
+//  ExerciseImageLoad.swift
+//  FItnessAppFrontend
+//
+//  Created by Sudeshika Gunawardena on 2023-05-19.
+//
+
+import UIKit
+
+class ExerciseImageLoad: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
